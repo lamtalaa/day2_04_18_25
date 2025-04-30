7f1df01cbbc640d1e73f6d22cb85bf61cc34bfe6
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2025-04-29 23:15:50 -0400

    updated interview questions plus adding calculator

---
+++ calculator.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+protocol calculatorOperations {
+    func addition(_ a: Double, _ b: Double) -> Double
+    func subtraction(_ a: Double, _ b: Double) -> Double
+    func multiplication(_ a: Double, _ b: Double) -> Double
+    func division(_ a: Double, _ b: Double) -> Double
+}
+
+class Calculator: calculatorOperations {
+    func addition(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
+    func subtraction(_ a: Double, _ b: Double) -> Double {
+        return a - b
+    }
+    
+    func multiplication(_ a: Double, _ b: Double) -> Double {
+        return a * b
+    }
+    
+    func division(_ a: Double, _ b: Double) -> Double {
+        return a / b
+    }
+}
+
+var calc: calculatorOperations = Calculator()
+print(calc.addition(10, 20))
+print(calc.subtraction(10, 20))
+print(calc.multiplication(10, 20))
+print(calc.division(10, 20))
